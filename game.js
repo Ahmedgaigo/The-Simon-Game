7cbec9509caba65584469afd59df659d896810b3
Ahmedgaigo <ahmetgaigo68@gmail.com>
2024-06-27 22:51:18 -0400

    update

--- game.js
+++ game.js
@@ -1,20 +1,37 @@
 var buttonColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
+var level = 0;
+var flag = false;
 
-// nextSequence();
 
-$(".btn").on("click", function(event){
+// --------- Starting the Game------------------
+$(document).on("keypress", function(){
+    if (!flag) {
+        $("h1").text("Level " + level);
+        nextSequence();
+        flag = true;
+    }
+});
+
+
+$(".btn").on("click", function(){
     var userChosenColor = $(this).attr("id");
     userClickedPattern.push(userChosenColor);
     // play sound
     playSound(userChosenColor);
     // animate
     animatePress(userChosenColor);
+    checkAnswer(userClickedPattern.length - 1);
 });
-
+  
 
 function nextSequence(){
+
+    userClickedPattern = [];
+    level++;
+    $("h1").text("Level " + level);
+
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
@@ -24,6 +41,7 @@ function nextSequence(){
     // sound
     playSound(randomChosenColor);
 }
+
 
 
 function playSound(name){
@@ -42,14 +60,38 @@ function animatePress(currentColor){
 
 
 
-
-
-
-
-
-
-
-
-
-
+function checkAnswer(currentLevel){
     
+    if (userClickedPattern[currentLevel] === gamePattern[currentLevel]){
+
+        console.log("success");
+
+        if (userClickedPattern.length === gamePattern.length){
+            setTimeout(function(){nextSequence();}, 1000);
+        }
+    }
+
+    else {
+
+        console.log("wrong");
+
+        playSound("wrong");
+
+        $("body").addClass("game-over");
+        
+        setTimeout(function(){
+            $("body").removeClass("game-over");
+        }, 200);
+
+        $("h1").text("Game Over, Press Any Key to Restart");
+
+        startOver();
+    }
+}
+
+
+function startOver(){
+    level = 0;
+    gamePattern = [];
+    flag = false;
+}
